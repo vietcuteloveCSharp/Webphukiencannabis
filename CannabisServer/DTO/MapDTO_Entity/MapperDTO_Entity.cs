188856765f7b690a97662d2bcdcaef8ccba3c630
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-06-23 14:56:36 +0700

    Update file MapperDTO_Entity

--- CannabisServer/DTO/MapDTO_Entity/MapperDTO_Entity.cs
+++ CannabisServer/DTO/MapDTO_Entity/MapperDTO_Entity.cs
@@ -9,14 +9,22 @@ using DTO.CustomerDataTransferObj;
 
 namespace DTO.MapDTO_Entity
 {
-	public class MapperDTO_Enity :Profile
+	public class MapperDTO_Entity :Profile
 	{
-		public MapperDTO_Enity()
+		public MapperDTO_Entity()
 		{
-			#region Map 
+			#region Map Customer
 			CreateMap<Customer, CustomerDTO>(MemberList.None)
 				.ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
 				.ForMember(dest => dest.Password, opt => opt.Ignore())
+				.ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
+				.ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
+				.ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone))
+				.ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
+				.ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+				.ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+			CreateMap<CustomerDTO, Customer>(MemberList.None)
+				.ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
 				.ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
 				.ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
 				.ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone))
