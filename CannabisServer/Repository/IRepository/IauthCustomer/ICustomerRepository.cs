038d1850446e482317db8c53550e550b6491f89d
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-06-23 15:00:22 +0700

    Add interface customer

---
+++ CannabisServer/Repository/IRepository/IauthCustomer/ICustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Entities;
+using DTO.CustomerDataTransferObj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.IRepository.IauthCustomer
+{
+	public interface ICustomerRepository
+	{
+
+		IAsyncEnumerable<Customer> GetAllCustomersAsync();
+		Task<Customer> GetCustomerByIdAsync(int id);		
+		Task<bool> EmailExistsAsync(string email);
+		Task<bool> UserNameExistsAsync(string userName);
+		Task<CustomerDTO> AddCustomerAsync(CustomerDTO customerDTO);
+
+	}
+}
